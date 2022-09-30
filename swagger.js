426996069ad674a21c15f800d20151c038459d1f
Poser <arr15004@byui.edu>
2022-09-29 18:31:45 -0600

    updated swagger title and description

--- swagger.js
+++ swagger.js
@@ -2,7 +2,7 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
   info: {
-    title: 'World Cup 2022',
+    title: 'Soccer World Cup 2022 Qatar',
     description: 'World Cup 2022 API'
   },
   host: 'carlosworldcup2022.onrender.com',
