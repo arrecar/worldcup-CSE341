a381983d670d23ee680f8e22b7206865ffeac055
Poser <arr15004@byui.edu>
2022-10-10 20:42:46 -0600

    updated the secret

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ mongodb.initDb((err, mongodb) => {
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: process.env.SECREt,
+  secret: process.env.SECRET,
   baseURL: process.env.BASE_URL,
   clientID: process.env.CLIENT_ID,
   issuerBaseURL: process.env.ISSUER_BASE_URL
